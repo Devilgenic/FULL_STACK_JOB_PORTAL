730235d93d4daf9122414dc23303577b3abd4bdb
Prashant Kumar <114344915+Devilgenic@users.noreply.github.com>
2024-09-17 22:32:33 +0530

    first commit

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+ const { default: mongoose } = require("mongoose");
+
+ const connectToDB = async () => {
+   const connectionURL = process.env.MONGODB_URL;
+
+  mongoose
+    .connect(connectionURL)
+     .then(() => console.log("job board database connection is successfull"))
+     .catch((error) => console.log(error));
+ };
+
+ export default connectToDB;
+
+
+
